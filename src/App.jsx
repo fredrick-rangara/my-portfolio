5f268ea6773b3de3d60a7ab922c42bc7197e8176
fredrck rangara <ybnfredae@gmail.com>
2026-02-26 14:41:12 +0300

    add function to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,3 +7,22 @@ import ProjectDetailPage from './pages/ProjectDetailPage';
 import NotFoundPage from './pages/NotFoundPage';
 import GlobalStyles from './styles/global';
 import { theme } from './styles/variables';
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/project/:id" element={<ProjectDetailPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
