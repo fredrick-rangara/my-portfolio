65ccc5dd1d5324e3dce69f3a5d8963258b9a780c
fredrck rangara <ybnfredae@gmail.com>
2026-02-26 14:42:37 +0300

    add function to Navbar.jsx

---
+++ src/components/Layout/Navbar.jsx
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { motion, AnimatePresence } from 'framer-motion';
+import { HiMenu, HiX, HiCode } from 'react-icons/hi';
+import styled from 'styled-components';
